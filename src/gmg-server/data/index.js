42b259c14a4245221f2713b965e0a59ba12a6ff6
Kelly Householder <kelly@Kellys-MacBook-Pro.local>
2020-05-15 13:55:15 -0600

    Fixed sqlite3 interface to create the database. Updated the modules to latest versions added launch code for NPM

--- src/gmg-server/data/index.js
+++ src/gmg-server/data/index.js
@@ -1,3 +1,4 @@
+const sqlite3 = require('sqlite3')
 const SQLite = require('sqlite')
 const Path = require('path')
 
@@ -7,8 +8,8 @@ module.exports.initialize = ({ logger }) => {
    const db_path = Path.join(__dirname, './grill_data.db')
 
    logger('Initializing db: [%s]', db_path)
-
-   db = SQLite.open(db_path, { Promise })
-}
+   //db = SQLite.open(db_path, { Promise })
+   db = SQLite.open({filename: db_path, driver: sqlite3.Database, mode: sqlite3.OPEN_CREATE | sqlite3.OPEN_READWRITE}, { Promise })
+   }
 
 module.exports.createDb = async () => db
